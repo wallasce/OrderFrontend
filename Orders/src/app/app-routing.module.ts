847bf78615e710677b6708b2c39659dadfbf5c4d
Wallasce Leite <lwallasce@outlook.com>
2024-05-06 10:41:26 -0300

    Feat: Create Routes

--- Orders/src/app/app-routing.module.ts
+++ Orders/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { NewCategoryComponent } from './new-category/new-category.component';
+import { EditCategoryComponent } from './edit-category/edit-category.component';
+
+const routes: Routes = [
+  {
+    path: 'categories',
+    component: CategoriesComponent,
+    data: {title: 'Categories'}
+  },
+  {
+    path: 'category/:id/details',
+    component: CategoryDetailsComponent,
+    data: {title: 'Category Details'}
+  },
+  {
+    path: 'category/new',
+    component: NewCategoryComponent,
+    data: {title: 'Create Category'}
+  },
+  {
+    path: 'category/:id/edit',
+    component: EditCategoryComponent,
+    data: {title: 'Edit Category'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

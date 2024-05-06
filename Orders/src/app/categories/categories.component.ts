dbf0ba40a2d4104b7eaf3c1d15af1540cd75389c
Wallasce Leite <lwallasce@outlook.com>
2024-05-06 17:05:55 -0300

    Feat: Use Order API in Categories Component

--- Orders/src/app/categories/categories.component.ts
+++ Orders/src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/model/category';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  dataSource : Category[] = [];
+  displayedColumns = [ 
+    'Name',
+    'Action'
+  ];
+  isLoadingResults = true;
 
-  constructor() { }
+  constructor(private api :ApiService) { }
 
   ngOnInit(): void {
+    this.api.getCategories().subscribe({
+      next: (res) => {
+        this.dataSource = res;
+        console.log(this.dataSource)
+      }, error: (err) => {
+        console.log(err);
+      }, complete: () => {
+        this.isLoadingResults = false;
+      }
+    });
   }
 
 }
